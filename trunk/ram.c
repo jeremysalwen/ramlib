9868bcc90824c7780ee134052fae7606cd19d5b5
jeremybubs <jeremybubs@4f77bd88-bc48-0410-96f9-cf51499bc5e1>
2010-02-03 00:47:15 +0000

    Clarified and expanded documentation.

--- trunk/ram.c
+++ trunk/ram.c
@@ -2,16 +2,30 @@
 *
 *	This is a program which computes the Riemann sums and trapezoid rule sums of an expression over
 *  a given interval with a given number of steps.  It is used for the estimation of the area under
-*  a curve, or the integral.  For information on Riemann sums, see
-*  http://en.wikipedia.org/wiki/Riemann_sum .  The entry point is _main() in ram.c.  Various
-*  functions dealing with popping or examining elements on the expression stack are located in
-*  jstack.c.  For a reentrant (besides temporarily enlarging the stack) function to call which does
-*  riemann sums or trapeziod rule calculations,
-*  see riemann_sum() or trap_rule() in ram.c.
+*  a curve, i.e. an integral.  For information on Riemann sums, see
+*  http://en.wikipedia.org/wiki/Riemann_sum .  The entry point is _main() in ram.c when run on
+*  a graphing calculator.  From the user's point of view, the program is a function, taking arguments
+*  and returning a result.  From the code's point of view, it must pop off the arguments from an
+*  "expression stack", in order to access the algebraic expressions it is passed.  It then pushes 
+*  the result back onto the stack.  This is useful as an executable on the calculator, but not as
+*  useful when called from other C or assembly language programs.
+*
+* For reentrant library functions which do Riemann sums and trapeziod rule calculations,
+*  see  riemann_sum() or trap_rule() in ram.c.
+* Various functions dealing with popping or examining elements on the expression stack are located in
+*  jstack.c.
+*
 * \section Arguments
-* 	Note: The arguments are taken from the expression stack on the calculator.
+*  The function call on the graphing calculator is of the form 
+*  <B> ramlib(expression, var, lower_bounds, upper_bounds, number_of_steps, function_index) </B>
+*
+* 	Note: The arguments are taken from the expression stack on the calculator. For the user, this
+*   simply means that they can be passed in the usual manner i.e. "ramlib(a,b,c..)".  However, in
+*   order to use the results as part of a further expression (i.e. "2*ramlib(a,b,c...)"), it might
+*   require the HW3 Patch, available from Kevin Kofler at
+*   http://www.tigen.org/kevin.kofler/ti89prog.htm
 *	\subsection expression
-*		The expression to be integrated.
+*		The function to be integrated
 * \subsection var
 *  The variable of integration.
 * \subsection lower_bounds lower bounds
@@ -22,7 +36,7 @@
 *  The number of sections to divide the region into; the number of sampling points.  Increasing
 *  	this will increase the accuracy, but will make the program take longer to execute.
 * \subsection function_index function index
-*  This determines which computation will be performed according to the following: \n
+*  This determines which computation will be performed according to the following table: \n
 *  		0 = left riemann sum \n
 *  		1 = middle riemann sum \n
 *  		2 = right riemann sum \n
@@ -77,9 +91,12 @@ float value_at_point(ESI expression, ESI var, float x) {
     return result;
 }
 /*!
-* Returns the rieman sum, for expression from lower_bounds to upper_bounds, with numsteps number
-*  of steps, using var as the variable of integration.  Assuming lram is the default operation,
-*  offset * dx (with var as x), would be added to the x coordinate of every sample point.
+* Returns the Riemann sum of the argument "expression".  The "var" argument is the variable which
+*  changes; it is analagus to the variable of integration. The bounds are determind by lower_bounds
+*  and upper_bounds, and the number of steps is determined by numsteps. 
+* Assuming left Riemann sums are the default operation, offset * dx (with var as x), will be added
+*  to the location of every sampling point.  This effectively shifts over the location on which the
+*  sum is being taken.  Thus, for example, one could do a right riemann sum by setting offset to 1.
 */
 float riemann_sum(ESI expression, ESI var, float lower_bounds, float upper_bounds,
 			unsigned int numsteps,float offset) {
@@ -92,8 +109,8 @@ float riemann_sum(ESI expression, ESI var, float lower_bounds, float upper_bound
 	return result*step;
 }
 /*!
-*	This function is very similar to rram, except that it computes the trapezoid rule instead, and
-*  has no argument offset, because there are no variations on the trapeziod rule.
+*	This function is very similar to riemann_sum, except that it computes the trapezoid rule instead,
+*  and *  has no "offset" argument, because there are no variations on the trapeziod rule.
 */
 float trap_rule(ESI expression, ESI var, float lower_bounds, float upper_bounds,
 		 unsigned int numsteps) {
